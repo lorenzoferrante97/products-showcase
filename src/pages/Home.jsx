ab6fb157cefd1639b9e940744b0bfe644dd463f7
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-11-06 11:31:47 +0100

    add: hero section + loaded hero images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
 export default function Home() {
   return (
     <>
-      <h1>Home</h1>
-      <div></div>
+      {/* ANCHOR HERO SECTION ------------------------------------------------------- */}
+      <section>
+        {/* NOTE - hero image */}
+        <div></div>
+
+        {/* NOTE - hero content */}
+        <div>
+          <h1 className='font-display-2 text-on-base text-pretty'>
+            I <span className='text-primary'>prodotti</span> che ami in un unico posto
+          </h1>
+        </div>
+      </section>
     </>
   )
 }
